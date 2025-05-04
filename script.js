9c728c82576fce1e35f06a89a86d50ecf460cf60
47gfx <117771075+4700gfx@users.noreply.github.com>
2025-05-04 13:01:43 -0400

    Adding Variables for Game Elements

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+//Score of Player 0 or 1 in Current Roll 
+const playerDisplay0 = document.getElementById('score--0')
+const playerDisplay1 = document.getElementById('score--1')
+const resetButton = document.querySelector('.btn-new')
+const rollButton = 
+const holdButton 
+//Dice Element 
+const dice = document.querySelector('.dice')
+
+
+
+//Intial State of the Game
+playerDisplay0.textContent = 0;
+playerDisplay1.textContent = 0;
+dice.classList.add('hidden')
+
+
+
+//Event Listener for Game Logic 
+
