1bf5be31937205863bfb3e69cb6e869275eb29f8
47gfx <117771075+4700gfx@users.noreply.github.com>
2025-05-12 13:00:07 -0400

    Adding Functionality on Reset Button

--- script.js
+++ script.js
@@ -21,52 +21,94 @@ const holdButton = document.querySelector('.btn--hold')
 
 //Player Scores 
 
-const finalScores = [0, 0]
-
-
 //Intial State of the Game
-let currentScore = 0;
-let activePlayer = 0;
-playerDisplay0.textContent = 0;
-playerDisplay1.textContent = 0;
-diceElement.classList.add('hidden')
+let finalScores, currentScore, playing, activePlayer;
 
+
+const initalize = function (){
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  finalScores = [0, 0]
+
+  playerDisplay0.textContent = 0;
+  playerDisplay1.textContent = 0;
+  diceElement.classList.add('hidden')
+
+  currentScore0.textContent = 0
+  currentScore1.textContent = 0
+
+  player0El.classList.remove('player--winner')
+  player1El.classList.remove('player--winner')
+  player0El.classList.add('player--active')
+  player1El.classList.remove('player--active')
+
+
+}
+
+initalize()
+
+//Functions for Logic 
+const switchPlayer = function(){
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active')
+  player1El.classList.toggle('player--active')
+  console.log(`Active Player has Switched to ${activePlayer}`)
+}
 
 
 //Event Listener for Game Logic 
 rollButton.addEventListener('click', function(){
-    //1. Generating Random Dice Roll 
-    const dice = Math.trunc(Math.random() * 6) + 1
+  if(playing){
+       //1. Generating Random Dice Roll 
+       const dice = Math.trunc(Math.random() * 6) + 1
 
-    //2. Display Dice Image 
-    diceElement.classList.remove('hidden')
-    diceElement.src = `dice-${dice}.png`
-    console.log(`A ${dice} has been rolled`)
-
-    
-    //3. Check if 1 Has Been Rolled 
-    if (dice !== 1){
-
-    //If the roll is not 1 add to the current score 
-    currentScore += dice
-    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-    }else{
-
-    //Switch The Player and Reset the Score 
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-    player0El.classList.toggle('player--active')
-    player1El.classList.toggle('player--active')
-    console.log(`Active Player has Switched to ${activePlayer}`)
-    }
-  })
-
-resetButton.addEventListener('click', function(){
+       //2. Display Dice Image 
+       diceElement.classList.remove('hidden')
+       diceElement.src = `dice-${dice}.png`
+       console.log(`A ${dice} has been rolled`)
+   
+       
+       //3. Check if 1 Has Been Rolled 
+       if (dice !== 1){
+   
+       //If the roll is not 1 add to the current score 
+       currentScore += dice
+       document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+       }else{
+         switchPlayer()
+       }
+  }
+  
 
 })
+
+
+
+resetButton.addEventListener('click', initalize)
 
 
 holdButton.addEventListener('click', function(){
-
-})
+  if (playing){
+    console.log('Hold Has Been Clicked')
+    //Add Current Score to the Active Player 
+    finalScores[activePlayer] += currentScore
+    document.getElementById(`score--${activePlayer}`).textContent = finalScores[activePlayer]
+    
+    //Check If Score If 100
+    if (finalScores[activePlayer] >= 100){
+      //Finish Game [True]
+      playing = false;
+      document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+      document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
+      diceElement.classList.add('hidden')
+    } else {
+      //Switch Player [False]
+      switchPlayer()
+    }
+    
+  }
+  
+})  
